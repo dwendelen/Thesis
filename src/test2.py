0bb05f38a0fb69557e26c94821d63db6dba6480c
Daan Wendelen <daanwendelen@gmail.com>
2014-05-17 02:27:39 +0200

    Testing order

--- src/test2.py
+++ src/test2.py
@@ -4,6 +4,11 @@ import numpy.linalg as la
 
 a = np.array([[201,202],[203,204]], dtype = np.float32)
 
+print a[0][0]
+print a[0][1]
+print a[1][0]
+print a[1][1]
+
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
@@ -12,15 +17,15 @@ a_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
 b_buf = cl.Buffer(ctx, mf.WRITE_ONLY, a.nbytes)
 
 prg = cl.Program(ctx, """
-    #pragma OPENCL EXTENSION cl_amd_printf: enable
-    __kernel void k1(__global float *a, __global float *b)
+    __kernel void sum(__global float *a, __global float *b)
     {   
         int i = get_global_id(0);
-        //printf("%f :: %d\\n", a[i], i);
-        b[i] = a[i]
+        b[i] = a[i];
     }""").build()
 
-prg.sum(queue, a.shape, None, a_buf, b_buf)
+prg.sum(queue, [4], None, a_buf, b_buf)
 
-c = np.zeros(4)
+c = np.array([[0,0, 0,0]], dtype = np.float32)
 cl.enqueue_copy(queue, c, b_buf)
+
+print c
