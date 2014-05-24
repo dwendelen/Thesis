48db28738eef66cce1345f19241e61b93105821e
Daan Wendelen <daanwendelen@gmail.com>
2014-05-24 20:58:25 +0200

    Filling in U and T

--- test/Kernel/Float16x16x16Test.py
+++ test/Kernel/Float16x16x16Test.py
@@ -5,6 +5,20 @@ from Kernel.Float16x16x16 import Factory
 class Float16x16x16Test(unittest.TestCase):
 
     def testSimple(self):
+        
+        T = np.zeros((1,2,3));
+        T[0, 0, 0] = 111;
+        T[0, 1, 0] = 121;
+        T[0, 0, 1] = 112;
+        T[0, 1, 1] = 122;
+        T[0, 0, 2] = 113;
+        T[0, 1, 2] = 123;
+        
+        U=[]
+        U.append(np.array([[1,2]]))
+        U.append(np.array([[1,2],[3,4]]))
+        U.append(np.array([[1,2],[3,4],[5,6]]))
+        
         kernel = Factory().create(U, T)
         kernel.run()
         
