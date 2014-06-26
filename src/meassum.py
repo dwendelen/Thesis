06c6b251acdc6f6d795cbbf8161ed831fa45aff5
Daan Wendelen <daanwendelen@gmail.com>
2014-06-26 11:18:42 +0200

    Last fixes

--- src/meassum.py
+++ src/meassum.py
@@ -12,8 +12,7 @@ from Kernel.Sum.Sum16 import Sum16
 cq = ContextQueue(profile = True)
 cq.init()
 
-#n = 64000005;
-n = 32000005;
+n = 64000005;
 a = np.array(np.random.rand(n), dtype=np.float32)
 
 mf = cl.mem_flags
@@ -23,10 +22,14 @@ sa = cl.Buffer(cq.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
 k = Sum16(cq)
 k.compile()
 k.init(sa, s)
+
+k.run()
+k.run()
 k.run()
 
 r = k.getSum()
-e = np.sum(a)
+e = np.sum(a[:16000000]) + np.sum(a[16000000:32000000]) \
+    + np.sum(a[32000000:48000000]) + np.sum(a[48000000:])
 
 print r
 print e
